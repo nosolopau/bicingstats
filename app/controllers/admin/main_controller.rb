3222208c599f918bf21e9622b702b47871fb0700
Pau <code@nuuve.com>
2011-10-29 14:26:52 +0200

    First working version

--- app/controllers/admin/main_controller.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-class Admin::MainController < ApplicationController
-  before_filter :require_admin
-
-  def index
-  end
-end
--- app/models/admin_session.rb
+++
@@ -1,7 +0,0 @@
-# -*- encoding : utf-8 -*-
-class AdminSession < Authlogic::Session::Base
-  include ActiveModel::Conversion
-  def persisted?
-    false
-  end
-end
